70be4efebe9fb0b0de8e1f54563e21ccc1eb3027
sayoyeh <loveweekly@gmail.com>
2021-04-24 18:50:10 +0800

    Second video - import images

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
+import './styles.css';
+import IMAGE from './RO.png';
+
 export const App = () => {
-    return <h1>Hello world.</h1>;
+    return <>
+        <h1>Hello world.</h1>
+        <img src={IMAGE} alt="RO" width="300" height="300"/>
+    </>;
 };
