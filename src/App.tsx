9b8ecb800f8b6208627eb6a3b42e34017e166209
sayoyeh <loveweekly@gmail.com>
2021-04-24 19:25:06 +0800

    Third video

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import IMAGE from './RO.png';
 
 export const App = () => {
     return <>
-        <h1>Hello world.</h1>
+        <h1>Hello world. {process.env.NODE_ENV} - {process.env.name}</h1>
         <img src={IMAGE} alt="RO" width="300" height="300"/>
     </>;
 };
