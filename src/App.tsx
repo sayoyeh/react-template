4f45e5e85ca4d77cc409e6a1ac798b038dbae84b
sayoyeh <loveweekly@gmail.com>
2021-04-24 23:09:06 +0800

    prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import "./styles.css";
-import IMAGE from "./RO.png";
+import './styles.css';
+import IMAGE from './RO.png';
 
 export const App = () => {
-  return (
-    <>
-      <h1>
-        Hello world. {process.env.NODE_ENV} - {process.env.name}
-      </h1>
-      <img src={IMAGE} alt="RO" width="300" height="300" />
-    </>
-  );
+    return (
+        <>
+            <h1>
+                Hello world. {process.env.NODE_ENV} - {process.env.name}
+            </h1>
+            <img src={IMAGE} alt="RO" width="300" height="300" />
+        </>
+    );
 };
